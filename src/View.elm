a2c81c978cf3243834752ccbc19b6632367bacba
nokijp <nokijp@users.noreply.github.com>
2020-08-10 02:41:18 +0900

    modify style

--- src/View.elm
+++ src/View.elm
@@ -10,26 +10,24 @@ import Model exposing (..)
 bodyContent : Model -> List (Html Msg)
 bodyContent model =
   let
-    headerOuterStyle =
-      css
-        [ padding2 (rem 3.0) zero
-        , backgroundColor (rgb 0x5f 0xab 0xdc)
-        , color (rgb 0xff 0xff 0xff)
-        ]
-    headerInnerStyle =
-      css
-        [ margin2 zero auto
-        , width (px 1000)
-        ]
-    formSection content = div [headerOuterStyle] [div [headerInnerStyle] content]
+    pageWidth = px 1000
+    primaryColor = rgb 0x33 0x99 0xff
+    accentColor = rgb 0xee 0xaa 0x00
+    primaryTextColor = rgb 0x55 0x55 0x55
+    secondaryTextColor = rgb 0x99 0x99 0x99
 
+    headerSectionStyle =
+      css
+        [ margin3 (rem 6.0) auto (rem 5.0)
+        , width pageWidth
+        , color primaryTextColor
+        ]
     mainTitleStyle =
       css
-        [ paddingBottom (rem 0.2)
+        [ paddingBottom (rem 0.4)
         , textTransform lowercase
         , fontSize (rem 1.5)
         ]
-    mainTitle = h1 [mainTitleStyle] [text "Elm Calc"]
 
     mainTextInputStyle =
       css
@@ -37,56 +35,51 @@ bodyContent model =
         , padding2 (rem 0.3) (rem 0.8)
         , width (pct 100)
         , outline zero
-        , border zero
-        , borderRadius (rem 0.2)
-        , backgroundColor (rgb 0x99 0xc7 0xe9)
-        , color (rgb 0xff 0xff 0xff)
+        , border3 (px 3.0) solid primaryColor
+        , borderRadius (rem 0.3)
+        , color inherit
         , fontSize (rem 2.0)
         , property "-webkit-appearance" "none"
         , pseudoElement "placeholder"
-            [ opacity (num 1.0)
-            ]
-        , focus
-            [ backgroundColor (rgb 0xff 0xff 0xff)
-            , color (rgb 0x33 0x33 0x33)
-            , pseudoElement "placeholder"
-                [ opacity (num 0.5)
-                ]
+            [ color (rgb 0xcc 0xcc 0xcc)
+            , opacity (num 1.0)
             ]
         ]
     messageStyle =
       css
         [ position absolute
-        , paddingTop (rem 0.2)
-        , color (rgb 0xbd 0xdf 0xff)
+        , paddingTop (rem 0.4)
+        , color secondaryTextColor
         ]
 
     historySectionStyle =
       css
         [ margin2 zero auto
         , padding2 (rem 1.0) zero
-        , width (px 1000)
-        , color (rgb 0x33 0x33 0x33)
+        , width pageWidth
+        , color primaryTextColor
         ]
     historySectionHeaderStyle =
       css
         [ displayFlex
+        , alignItems center
         ]
     historySectionTitleStyle =
       css
-        [ color (rgb 0x99 0x99 0x99)
-        , fontSize (rem 1.0)
+        [ color secondaryTextColor
+        , fontSize (rem 1.2)
         ]
     historyClearButtonStyle =
       css
-        [ marginLeft auto
+        [ marginLeft (rem 0.5)
         , padding2 (rem 0.1) (rem 0.5)
         , outline zero
-        , border3 (px 1.0) solid (rgb 0x99 0x99 0x99)
+        , border3 (px 1.0) solid secondaryTextColor
         , borderRadius (rem 0.1)
         , backgroundColor transparent
         , color (rgb 0x99 0x99 0x99)
         , fontSize (rem 0.8)
+        , cursor pointer
         , property "-webkit-appearance" "none"
         , hover
             [ backgroundColor (rgb 0x99 0x99 0x99)
@@ -100,8 +93,8 @@ bodyContent model =
     historyListItemStyle =
       css
         [ displayFlex
-        , margin3 (rem 0.5) zero (rem 1.5)
         , alignItems center
+        , margin3 (rem 1.0) zero (rem 1.5)
         ]
     variableLabelStyle =
       css
@@ -111,7 +104,7 @@ bodyContent model =
         , width (rem 3.5)
         , lineHeight (rem 1.1)
         , borderRadius (rem 0.1)
-        , backgroundColor (rgb 0xe9 0xb0 0x00)
+        , backgroundColor accentColor
         , textAlign center
         , color (rgb 0xff 0xff 0xff)
         , fontSize (rem 0.8)
@@ -124,8 +117,8 @@ bodyContent model =
          CalcResultOk r -> "= " ++ String.fromFloat r
   in
     [ Reset.meyerV2
-    , formSection
-        [ mainTitle
+    , div [headerSectionStyle]
+        [ h1 [mainTitleStyle] [text "Elm Calc"]
         , form [onSubmit TryToPush]
           [ input
               [ mainTextInputStyle
